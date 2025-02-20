d97fe280df4bba2cc98ad8ea847fca4d7cff24f3
andres2901 <49736604+andres2901@users.noreply.github.com>
2025-02-20 10:19:36 -0500

    Update 3_Trinotate.sh
    changes

--- 3_Annotation/3_Trinotate.sh
+++ 3_Annotation/3_Trinotate.sh
@@ -12,7 +12,7 @@
 #
 ########################################################################################
 
-assembly_path=$1 #path to the assembly file
+PREFIX=$1 #prefix of files for annotation
 output_name=$2 #name for output
 
 # ################## Module Loading Area ############################################
@@ -22,17 +22,25 @@ source activate trinotate
 
 ########################################################################################
 
-blastx -query ${SPECIE}.transcripts.cdna.fa -db /hpcfs/home/ciencias_biologicas/af.lizcano/database/Trinotate2/uniprot_sprot.pep -num_threads 8 -max_target_seqs 1 -outfmt 6 -evalue 1e-5 > ${SPECIE}_cdna_uniprot.blastx 2> ${SPECIE}_cdna_uniprot.blastx.err 
+blastx -query ${PREFIX}.transcripts.cdna.fa -db /hpcfs/home/ciencias_biologicas/af.lizcano/database/Trinotate2/uniprot_sprot.pep -num_threads 8 -max_target_seqs 1 -outfmt 6 -evalue 1e-5 > ${PREFIX}_cdna_uniprot.blastx 2> ${PREFIX}_cdna_uniprot.blastx.err 
 
-blastp -query ${SPECIE}.proteins.fa -db /hpcfs/home/ciencias_biologicas/af.lizcano/database/Trinotate2/uniprot_sprot.pep -num_threads 8 -max_target_seqs 1 -outfmt 6 -evalue 1e-5 > ${SPECIE}_proteins_uniprot.blastp 2> ${SPECIE}_proteins_uniprot.blastp.err 
+blastp -query ${PREFIX}.proteins.fa -db /hpcfs/home/ciencias_biologicas/af.lizcano/database/Trinotate2/uniprot_sprot.pep -num_threads 8 -max_target_seqs 1 -outfmt 6 -evalue 1e-5 > ${PREFIX}_proteins_uniprot.blastp 2> ${PREFIX}_proteins_uniprot.blastp.err 
 
-hmmscan --cpu 8 --noali --domtblout ${SPECIE}_pfam_HMMer.out /hpcfs/home/ciencias_biologicas/af.lizcano/database/Trinotate2/Pfam-A.hmm ${SPECIE}.proteins.fa > ${SPECIE}_pfam_HMMer.log 
+hmmscan --cpu 8 --noali --domtblout ${PREFIX}_pfam_HMMer.out /hpcfs/home/ciencias_biologicas/af.lizcano/database/Trinotate2/Pfam-A.hmm ${PREFIX}.proteins.fa > ${PREFIX}_pfam_HMMer.log 
 
-/hpcfs/home/ciencias_biologicas/af.lizcano/tmhmm-2.0c/bin/tmhmm --short < ${SPECIE}.proteins.fa > ${SPECIE}_tmhmm.out 2> ${SPECIE}_tmhmm.log 
+/hpcfs/home/ciencias_biologicas/af.lizcano/tmhmm-2.0c/bin/tmhmm --short < ${PREFIX}.proteins.fa > ${PREFIX}_tmhmm.out 2> ${PREFIX}_tmhmm.log 
 
 conda deactivate
 source activate signalp
 
-signalp6 --fastafile ${SPECIE}.proteins.fa --output_dir ${SPECIE}_signalp --organism eukarya
+signalp6 --fastafile ${PREFIX}.proteins.fa --output_dir ${PREFIX}_signalp --organism eukarya
+
+/hpcfs/home/ciencias_biologicas/af.lizcano/Trinotate-Trinotate-v4.0.2/Trinotate /hpcfs/home/ciencias_biologicas/af.lizcano/database/Trinotate2/TrinotateBoilerplate.sqlite init --gene_trans_map ${PREFIX}_geneTranscriptMap.txt --transcript_fasta ${PREFIX}_cdna.fa --transdecoder_pep ${PREFIX}_ProteinsTrinotate.fa
+/hpcfs/home/ciencias_biologicas/af.lizcano/Trinotate-Trinotate-v4.0.2/Trinotate /hpcfs/home/ciencias_biologicas/af.lizcano/database/Trinotate/Trinotate.sqlite LOAD_swissprot_blastp ${PREFIX}_proteins_uniprot.blastp
+/hpcfs/home/ciencias_biologicas/af.lizcano/Trinotate-Trinotate-v4.0.2/Trinotate /hpcfs/home/ciencias_biologicas/af.lizcano/database/Trinotate/Trinotate.sqlite LOAD_swissprot_blastx ${PREFIX}_cdna_uniprot.blastx
+/hpcfs/home/ciencias_biologicas/af.lizcano/Trinotate-Trinotate-v4.0.2/Trinotate /hpcfs/home/ciencias_biologicas/af.lizcano/database/Trinotate/Trinotate.sqlite LOAD_pfam ${PREFIX}_pfam_HMMer.out
+/hpcfs/home/ciencias_biologicas/af.lizcano/Trinotate-Trinotate-v4.0.2/Trinotate /hpcfs/home/ciencias_biologicas/af.lizcano/database/Trinotate/Trinotate.sqlite LOAD_tmhmm ${PREFIX}_tmhmm.out
+/hpcfs/home/ciencias_biologicas/af.lizcano/Trinotate-Trinotate-v4.0.2/Trinotate /hpcfs/home/ciencias_biologicas/af.lizcano/database/Trinotate/Trinotate.sqlite report > ${PREFIX}_trinotateReport.txt
+/hpcfs/home/ciencias_biologicas/af.lizcano/Trinotate-Trinotate-v4.0.2/util/report_summary/trinotate_report_summary.pl ${PREFIX}_trinotateReport.txt ${PREFIX}_trinotateReport
 
 ########################################################################################
