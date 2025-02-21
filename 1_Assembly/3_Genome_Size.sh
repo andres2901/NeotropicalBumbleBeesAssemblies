34995b8ed8407822125e2c25c093d41623401620
andres2901 <49736604+andres2901@users.noreply.github.com>
2025-02-21 10:02:08 -0500

    Update 3_Genome_Size.sh
    corrections

--- 1_Assembly/3_Genome_Size.sh
+++ 1_Assembly/3_Genome_Size.sh
@@ -2,7 +2,7 @@
 
 # ###### Parameters area for resource request to SLURM ############################
 #
-#SBATCH --job-name=InitialQuality	#Job Name.
+#SBATCH --job-name=GenomeSize	#Job Name.
 #SBATCH -p short			#Partition/queue in which to run the job.
 #SBATCH -N 1				#Number of nodes required.
 #SBATCH -n 1				#Parallel task.
@@ -21,6 +21,6 @@ source activate kat
 
 ########################################################################################
 
-kat hist -o k21 -t 8 -m 21 $file_path
-kat hist -o k27 -t 8 -m 27 $file_path
-kat hist -o k31 -t 8 -m 31 $file_path
+kat hist -o k21 -t 8 -m 21 $file_path #Determine genome size wit k-mer length of 21
+kat hist -o k27 -t 8 -m 27 $file_path #Determine genome size wit k-mer length of 27
+kat hist -o k31 -t 8 -m 31 $file_path #Determine genome size wit k-mer length of 31
